26d8ce5ecf2e70d1e70928a628fce3647c9da670
Vilson Borges <vilsonborges98@gmail.com>
2021-03-07 19:31:13 -0300

    first app with node

--- aulas/backend/src/index.js
+++ aulas/backend/src/index.js
@@ -1,45 +1,67 @@
 const express = require('express');
+const { uuid } = require('uuidv4');
 
 const app = express();
 
 app.use(express.json());
 
+const projects = [];
+
 app.get('/projects', (request, response) => {
-  const { title, owner } = request.query;
+  const { title } = request.query;
 
-  console.log(title);
-  console.log(owner);
-
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-  ]);
+  const results = title ? 
+    projects.filter(project => project.title.includes(title)) : 
+    projects;
+  
+  return response.json(results);
 });
 
 app.post('/projects', (request, response) => {
   const { title, owner} = request.body;
 
+  const project = { id: uuid(), title, owner}
+
+  projects.push(project);
+
   console.log(title, owner);
 
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3',
-  ]);
+  return response.json(project);
 });
 
 app.put('/projects/:id', (request, response) => {
-  return response.json([
-    'Projeto 4',
-    'Projeto 2',
-    'Projeto 3',
-  ]);
+  const { id } = request.params;
+  const { title, owner} = request.body;
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if( projectIndex < 0 ) {
+    console.log(projectIndex);
+    return response.status(400).json({ error: "project not found"});
+  }
+
+  const project = {
+    id,
+    title,
+    owner
+  };
+
+  projects[projectIndex] = project;
+
+  return response.json( project );
 });
 
 app.delete('/projects/:id', (request, response) => {
-  return response.json([
-    'Projeto 2',
-  ]);
+  const { id } = request.params;
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  if( projectIndex < 0 ) {
+    console.log(projectIndex);
+    return response.status(400).json({ error: "project not found"});
+  }
+
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.listen(3333, () => {
