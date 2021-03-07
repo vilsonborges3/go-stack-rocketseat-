8ff7500bd6cddb6bfd483a69342e5f116e39505d
Vilson Borges <vilsonborges98@gmail.com>
2021-03-07 18:49:02 -0300

    main types of parameters

--- aulas/backend/src/index.js
+++ aulas/backend/src/index.js
@@ -2,7 +2,14 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/projects', (request, response) => {
+  const { title, owner } = request.query;
+
+  console.log(title);
+  console.log(owner);
+
   return response.json([
     'Projeto 1',
     'Projeto 2',
@@ -10,6 +17,10 @@ app.get('/projects', (request, response) => {
 });
 
 app.post('/projects', (request, response) => {
+  const { title, owner} = request.body;
+
+  console.log(title, owner);
+
   return response.json([
     'Projeto 1',
     'Projeto 2',
