2f0f6b13585e9ca728ee76c9f9db03965cf22617
Vilson Borges <vilsonborges98@gmail.com>
2021-03-13 08:55:29 -0300

    props

--- aulas/frontend/src/App.js
+++ aulas/frontend/src/App.js
@@ -5,8 +5,20 @@ import Header from './componets/Header';
 function App() {
   return (
     <>
-      <Header />
-      <Header />
+      <Header title="Homepage">
+        <ul>
+          <li>Home</li>
+          <li>Projects</li>
+        </ul>
+      </Header>
+      <Header  title="Projects">
+        <ul>
+          <li>Home</li>
+          <li>Login</li>
+          <li>Sign in</li>
+          <li>Sign up</li>
+        </ul>
+      </Header> 
     </>
     );
 }
