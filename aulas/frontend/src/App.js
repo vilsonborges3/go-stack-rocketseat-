019ddfbe4cf9a0e7f186661a735e2cf8d4276b0e
Vilson Borges <vilsonborges98@gmail.com>
2021-03-17 12:52:02 -0300

    registering projects

--- aulas/frontend/src/App.js
+++ aulas/frontend/src/App.js
@@ -14,11 +14,18 @@ function App() {
     })
   }, [])
 
-  function handleAddProject() {
+  async function handleAddProject() {
     //projects.push(`Novo projeto ${Date.now()}`);
 
-    setProjects([...projects, `Novo projeto ${Date.now()}`]);
-    console.log(projects);
+    //setProjects([...projects, `Novo projeto ${Date.now()}`]);
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: "Diego Fernandes"
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
   }
 
   return (
